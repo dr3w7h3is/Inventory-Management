4155ece508b7ccbc86ee117b1d9bd61948162038
mrogers <12655691+SilentViking@users.noreply.github.com>
2019-12-12 22:09:51 -0800

    Added get method to server. Use get to retrieve db.

--- src/collector.js
+++ src/collector.js
@@ -1,41 +1,59 @@
-var http = require('http');
-var fs = require('fs');
-var dbFile = process.cwd() + '/src/fake-data.json'
+var http = require("http");
+var fs = require("fs");
+var dbFile = process.cwd() + "/src/fake-data.json";
 
+var db = initdb();
 
-var db = initdb()
+var data = "";
+http
+  .createServer(function(req, res) {
+    res.setHeader("Content-Type", "application/json" );
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    let body = "";
 
-var data = '';
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    let body = '';
-    req.on('data', (chunk) => { body += chunk })
-    req.on('end', a => {
+    if (req.method === "POST") {
+      req.on("data", chunk => {
+        body += chunk;
+      });
+      req.on("end", a => {
         res.write(body);
-        someThing(body);
+        handlePost(body);
         res.end();
-    })
-}).listen(8080);
+      });
+    } else if (req.method === "GET") {
+      if (req.url.toLowerCase() === "/dump") {
+        res.end(JSON.stringify(db));
+      }
+      if (req.url.toLowerCase() === "/remove") {
+        //
+        res.statusCode = 501;
+        res.end();
+      }
+    } else {
+      res.statusCode = 405;
+      res.end();
+    }
+  })
+  .listen(8080);
 
-function someThing(data) {
-    js = JSON.parse(data)
-    var cNum = db.database.length;
-    js.ctrl_num = cNum + 1;
-    db.database.push(js)
-    fs.writeFileSync(dbFile,JSON.stringify(db),'utf8')
+function handlePost(data) {
+  js = JSON.parse(data);
+  var cNum = db.database.length;
+  js.ctrl_num = cNum + 1;
+  db.database.push(js);
+  fs.writeFileSync(dbFile, JSON.stringify(db), "utf8");
 }
 
-
 function initdb() {
-    var raw = ''
-    if (fs.existsSync(dbFile)) {
-        var raw = fs.readFileSync(dbFile, 'utf8')
-    }
-    
-    if (raw === '') {
-        raw = {
-            "database":[]
-        }
-    }
-    return (typeof raw === 'string') ? JSON.parse(raw) : raw
+  var raw = "";
+  if (fs.existsSync(dbFile)) {
+    var raw = fs.readFileSync(dbFile, "utf8");
+  }
+
+  if (raw === "") {
+    raw = {
+      database: []
+    };
+  }
+  return typeof raw === "string" ? JSON.parse(raw) : raw;
 }
