f579d35017c842c32809dfa4f726c1c5502152fa
SilentViking <12655691+SilentViking@users.noreply.github.com>
2019-12-17 13:14:48 -0800

    seperate componentns

---
+++ src/client/components/CheckOut.js
@@ -0,0 +1,9 @@
+import React from "react";
+// Function to create elements in the checkout page
+export class CheckOut extends React.Component {
+    render() {
+        return (<div className="main-body w3-container">
+            <h1>CheckOut</h1>
+        </div>);
+    }
+}
